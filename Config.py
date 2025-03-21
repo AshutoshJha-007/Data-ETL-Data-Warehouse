4adf7c8cca8fdbeb8aa8b799d107a5f712a0aa49
ASHUTOSH KUMAR JHA <95684293+AshutoshJha-007@users.noreply.github.com>
2025-03-21 09:21:53 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,11 @@
+# config.py
+
+AWS_ACCESS_KEY = "your_aws_access_key"
+AWS_SECRET_KEY = "your_aws_secret_key"
+S3_BUCKET = "your-s3-bucket-name"
+
+DB_USER = "your_db_username"
+DB_PASSWORD = "your_db_password"
+DB_HOST = "your-redshift-cluster-endpoint"
+DB_PORT = "5439"
+DB_NAME = "your_database_name"
